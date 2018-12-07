67ede9c6264176888e0a1e7184e1fa6c60edeee6
HouZengYuHou <2393325422@qq.com>
2018-12-07 16:41:43 +0800

    第一次提交

---
+++ src/main/java/com/aidong/APP2.java
@@ -0,0 +1,11 @@
+package com.aidong;
+
+import org.testng.annotations.Test;
+
+public class APP2 {
+    @Test
+    public void test_4(){
+    assert 1==1;
+    System.out.println("test_4");
+    }
+}
